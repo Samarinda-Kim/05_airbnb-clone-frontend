2937c8efc934b001394fde70a49c2ad55d863498
Samarinda-Kim <ittsgoodman@gmail.com>
2023-02-05 01:02:35 +0900

    #18.5 Responsive Design

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,42 +1,64 @@
-import { FaStar } from 'react-icons/fa'
-import { Box, Grid, Text, HStack, Image, VStack } from '@chakra-ui/react'
+import {
+  Box,
+  Grid,
+  Text,
+  HStack,
+  Image,
+  VStack,
+  Button,
+} from '@chakra-ui/react'
+import Room from '../components/Room'
 
 export default function Home() {
   return (
     <Grid
       mt={10}
-      px={40}
+      px={{
+        base: 10,
+        lg: 40,
+      }}
       columnGap={4}
       rowGap={8}
-      templateColumns={'repeat(5, 1fr)'}
+      templateColumns={{
+        sm: '1fr',
+        md: '1fr 1fr',
+        lg: '1fr 1fr 1fr',
+        xl: 'repeat(4, 1fr)',
+        '2xl': "'repeat(5, 1fr)',",
+      }}
     >
-      <VStack alignItems={'flex-start'}>
-        <Box>
-          <Box overflow={'hidden'} mb={3} rounded={'3xl'}>
-            <Image
-              h="280"
-              src="https://a0.muscache.com/im/pictures/prohost-api/Hosting-553863823287705078/original/f2369864-c9eb-4222-a73a-018e55254aed.jpeg?im_w=720"
-            />
-          </Box>
-        </Box>
-        <Box>
-          <Grid gap={2} templateColumns={'6fr 1fr'}>
-            <Text as="b" noOfLines={1} fontSize="md">
-              Ganggu-myeon, Yeongdeok-gun, 경상북도, 한국
-            </Text>
-            <HStack spacing={1}>
-              <FaStar size={15} />
-              <Text>5.0</Text>
-            </HStack>
-          </Grid>
-          <Text fontSize={'sm'} color="gray.600">
-            Seoul, S. Korea
-          </Text>
-        </Box>
-        <Text fontSize={'sm'} color="gray.600">
-          <Text as="b">$72</Text> / night
-        </Text>
-      </VStack>
+      {[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+      ].map((index) => (
+        <Room key={index} />
+      ))}
     </Grid>
   )
 }
