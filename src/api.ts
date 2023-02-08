e7c913b335a31b7d7f41171015064638e0ae5bb8
Samarinda-Kim <ittsgoodman@gmail.com>
2023-02-09 00:03:07 +0900

    #19.3 Axios

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,7 @@
+import axios from "axios"
 
-const BASE_URL = "http://127.0.0.1:8000/api/v1"
+const instance = axios.create({
+    baseURL: "http://127.0.0.1:8000/api/v1/"
+})
 
-export async function getRooms() {
-    const response = await fetch(`${BASE_URL}/rooms/`)
-    const json = await response.json()
-    return json;
-}
+export const getRooms = () => instance.get("rooms/").then((response) => response.data)
