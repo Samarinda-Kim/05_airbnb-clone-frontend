0125f0dcac17b8f56ddf54e58b24e0ee1b88a165
Samarinda-Kim <ittsgoodman@gmail.com>
2023-03-01 01:07:00 +0900

    #20.4 CSRF

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,6 +21,7 @@ import LoginModal from './LoginModal'
 import SignUpModal from './SignUpModal'
 import useUser from '../lib/useUser'
 import { logOut } from '../api'
+import { useQueryClient } from '@tanstack/react-query'
 
 export default function Header() {
   const { userLoading, isLoggedIn, user } = useUser()
@@ -33,6 +34,7 @@ export default function Header() {
   const logoColor = useColorModeValue('red.500', 'red.200')
   const Icon = useColorModeValue(FaMoon, FaSun)
   const toast = useToast()
+  const queryClient = useQueryClient()
   const onLogOut = async () => {
     const toastId = toast({
       title: 'Login out...',
@@ -40,15 +42,15 @@ export default function Header() {
       status: 'loading',
       position: 'bottom-right',
     })
-    // const data = await logOut()
-    // console.log(data)
+    await logOut()
+    queryClient.refetchQueries(['me'])
     setTimeout(() => {
       toast.update(toastId, {
         status: 'success',
         title: 'Done!',
         description: 'See you later!',
       })
-    }, 3000)
+    }, 1000)
   }
   const {
     isOpen: isSignUpOpen,
